2e5b5a4cc54e89566942c8b54b4bf77ae7c709b9
Deadzame <88514763+Deadzame@users.noreply.github.com>
2021-09-12 22:03:48 +0300

    Поставил комменты на нужные строки

--- reallife.js
+++ reallife.js
@@ -1,12 +1,13 @@
 mp.game.ped.removeScenarioBlockingArea(0,true);
 mp.game.streaming.setPedPopulationBudget(3);
-mp.game.ped.setCreateRandomCops(true);
-mp.game.vehicle.setRandomBoats(true);
-mp.game.vehicle.setRandomTrains(true);
-mp.game.vehicle.setGarbageTrucks(true);
+mp.game.ped.setCreateRandomCops(true); // Рандомные менты на карте( может пройти мини процесс стрелки)
+mp.game.vehicle.setRandomBoats(true); // Рандомные NPC катаются по карте
+mp.game.vehicle.setRandomTrains(true); // Рандомные поезда( нет смысла не доделана синхра)
+mp.game.vehicle.setGarbageTrucks(true); // Поставить мусоровозов на карту
 mp.game.streaming.setVehiclePopulationBudget(3);
-mp.game.invoke('0x34AD89078831A4BC'); // SET_ALL_VEHICLE_GENERATORS_ACTIVE
+mp.game.invoke('0x34AD89078831A4BC'); // Поставить генератор авто, на активный уровень
 mp.game.vehicle.setAllLowPriorityVehicleGeneratorsActive(true);
 mp.game.vehicle.setNumberOfParkedVehicles(-1);
 mp.game.vehicle.displayDistantVehicles(true);
 mp.game.graphics.disableVehicleDistantlights(false);
+// Закоментированно самое главное, остальное на среднечке, главный баг будет в том что при нажатии F1 не будет работать та меню, но кому надо - пофиксит!
